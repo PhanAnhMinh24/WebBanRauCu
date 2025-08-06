575ffb3a71e54daad83f901b683ad69ccd56bb1e
Hoangg205 <nguyenhoangksor2005@gmail.com>
2025-08-06 17:52:22 +0700

    Đẩy web lên

---
+++ hocphp/test.php
@@ -0,0 +1,17 @@
+<?php
+// define("Hoanggg",["Thien", "ly", "oi"]);
+// echo Hoanggg[1];
+$x=15;
+$y=7;
+echo $x + $y;
+echo ("<br>");
+echo $x - $y;
+echo ("<br>");
+echo $x * $y;
+echo ("<br>");
+echo $x / $y;
+echo ("<br>");
+echo $x % $y;
+echo ("<br>");
+echo $x ** $y;
+?>
